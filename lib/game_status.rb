d5c724016a1cdf29d61bc7b1db4adebbdca7c47d
Tom Shephard <tom.shephard@hotmail.co.uk>
2020-09-30 09:21:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ WIN_COMBINATIONS = [
 
 # Use a .detect enumerator?
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
+  WIN_COMBINATIONS.detect do |win_combination|
 
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
